8a6f0a9794e22d7f4b1eaf0d7b93f9ec17648d93
0xkuwabatake <0xkuwabatake@gmail.com>
2024-11-27 15:47:16 +0700

    initial commit

---
+++ src/SSTORE2Factory.sol
@@ -0,0 +1,230 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {SSTORE2} from "lib/solady/src/utils/SSTORE2.sol";
+
+/**
+ * @title  SSTORE2Factory
+ * @author 0xkuwabatake(0xkuwabatake)
+ * @notice A factory contract to deploy any arbitrarily `data` with or without `salt`
+ *         as storage contract.
+ * @dev    All of the methods of this contract are just external or public visibility method
+ *         to utilize Solady-SSTORE2 library by vectorized.eth:
+ *         https://github.com/Vectorized/solady/blob/main/src/utils/SSTORE2.sol
+ *        
+ * @custom:notes
+ * - The main purpose of this contract is to get the storage contract address or `pointer` 
+ *   from each `data` either with or without `salt` (it depends on the method being called)
+ *   that been deployed as storage contract and then store its value at another contract 
+ *   independently from this contract.
+ *   Because of that reason, neither storage provided by this contract to store the `pointer`
+ *   nor a tracker to track if a `pointer` from a `data` either with or without `salt`
+ *   had been deployed or not.
+ * - To get the value of `pointer` with its `index` (it depends on the method being called) 
+ *   are queryable via emitted {Pointer} event from each successful transaction.
+ * - Every state changing methods are marked as payable merely for the sake of gas optimisation,
+ *   meaning the caller does NOT need to send ether to this contract when sending a transaction.
+ *   Therefore, make sure to always set msg.value to 0 (zero) before calling a method.
+ * 
+ * @custom:warning
+ * - This contract has a capability to receive ether, therefore a withdrawal function to `_RECEIVER` 
+ *   is provided just in case the ether balance of this contract is non-zero. 
+ *   But, it does not mean the `_RECEIVER` has an obligation to refund the received ether 
+ *   to whichever caller that had sent ether to this contract either intentionally or not.
+ */
+contract SSTORE2Factory { 
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANT
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Ether receiver address.
+    address private constant _RECEIVER = 0xfa98aFe34D343D0e63C4C801EBce01d9D4459ECa; /* ACHTUNG ! */
+
+    /*//////////////////////////////////////////////////////////////
+                            CUSTOM EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Emitted when data is written as storage contract and return its `pointer` with `index`.
+    event Pointer(uint256 index, address pointer);
+
+    /// @dev Emitted when data is written as storage contract and return its `pointer`.
+    event Pointer(address pointer);
+
+    /*//////////////////////////////////////////////////////////////
+                            CUSTOM ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Revert with an error if {withdraw} function being called not by `_RECEIVER` address.
+    error Unauthorized();
+
+    /// @dev Revert with an error if length of array for `data` and `salt` arguments is mismatch.
+    error ArrayLengthMismatch();
+
+    /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev To save some gas during deployment.
+    constructor() payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                        RECEIVE & FALLBACK
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev To silence the compiler warning because of {fallback} function.
+    receive() external payable {}
+
+    /// @dev It's provided to receive calldata directly from a transaction.
+    fallback() external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                        EXTERNAL PAYABLE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Deploy `data` as a storage contract.
+     * @param data The data that will be deployed via {SSTORE2 - write}.
+     */
+    function write(bytes calldata data) external payable {
+        address pointer = SSTORE2.write(data);
+        emit Pointer(pointer);
+    }
+ 
+    /**
+     * @dev Deploy `data` as storage contract in batch.
+     * @param data The data that will be deployed via {SSTORE2 - write}.
+     */
+    function write(bytes[] calldata data) external payable {
+        uint256 i;
+        do {
+            address pointer = SSTORE2.write(data[i]);
+            emit Pointer(i, pointer);
+            unchecked { ++i; }
+        } while (i != data.length);
+    }
+ 
+    /**
+     * @dev Deploy `data` as a storage contract with `salt` and returns its counterfactual `pointer`.
+     * @param data The data that will be deployed via {SSTORE2 - writeCounterFactual}.
+     * @param salt The nonce that will be passed into {SSTORE2 - writeCounterfactual}.
+     * @custom:note Once `data` with specific `salt` is deployed, it can't not be redeployed.
+     */
+    function writeCounterfactual(bytes calldata data, bytes32 salt) external payable {
+        address pointer = SSTORE2.writeCounterfactual(data, salt);
+        emit Pointer(pointer);
+    }
+
+    /**
+     * @dev Deploy `data` as storage contract with `salt` in batch 
+     * and returns its counterfactual `pointer`.
+     * @param data  The data that will be deployed via {SSTORE2 - writeCounterFactual}.
+     * @param salt  The nonce that will be passed into {SSTORE2 - writeCounterfactual}.
+     * @custom:note Once `data` with specific `salt` is deployed, it can't not be redeployed.
+     */
+    function writeCounterfactual(bytes[] calldata data, bytes32[] calldata salt) external payable {
+        if (data.length != salt.length) revert ArrayLengthMismatch();
+        uint256 i;
+        do {
+            address pointer = SSTORE2.writeCounterfactual(data[i], salt[i]);
+            emit Pointer(i, pointer);
+            unchecked { ++i; }
+        } while (i != data.length);
+    }
+
+    /**
+     * @dev Deploy `data` as a storage contract with `salt` and returns its deterministic `pointer`.
+     * @param data The data that will be deployed via {SSTORE2 - writeDeterministic}.
+     * @param salt The nonce that will be passed into {SSTORE2 - writeDeterministic}.
+     * @custom:note `salt` is no longer usable if it had been used to deploy `data` deterministically.
+     */
+    function writeDeterministic(bytes calldata data, bytes32 salt) external payable {
+        address pointer = SSTORE2.writeDeterministic(data, salt);
+        emit Pointer(pointer);
+    }
+ 
+    /**
+     * @dev Deploy `data` as a storage contract with `salt` in batch 
+     * and returns its deterministic `pointer`.
+     * @param data The data that will be deployed via {SSTORE2 - writeDeterministic}.
+     * @param salt The nonce that will be passed into {SSTORE2 - writeDeterministic}.
+     * @custom:note `salt` is no longer usable if it had been used to deploy `data` deterministically.
+     */
+    function writeDeterministic(bytes[] calldata data, bytes32[] calldata salt) external payable {
+        if (data.length != salt.length) revert ArrayLengthMismatch();
+        uint256 i;
+        do {
+            address pointer = SSTORE2.writeDeterministic(data[i], salt[i]);
+            emit Pointer(i, pointer);
+            unchecked { ++i; }
+        } while (i != data.length);
+    }
+
+    /**
+     * @dev Withdraw all of ether balance from contract to `_RECEIVER`.
+     * @custom:note See: https://github.com/Vectorized/solady/blob/main/src/utils/SafeTransferLib.sol
+     */
+    function withdraw() external payable {
+        if (msg.sender != _RECEIVER) revert Unauthorized();
+        /// @solidity memory-safe-assembly
+        assembly {
+            // forgefmt: disable-next-item
+            if iszero(call(100000, _RECEIVER, selfbalance(), codesize(), 0x00, codesize(), 0x00)) {
+                mstore(0x00, _RECEIVER) // Store the address in scratch space.
+                mstore8(0x0b, 0x73) // Opcode `PUSH20`.
+                mstore8(0x20, 0xff) // Opcode `SELFDESTRUCT`.
+                if iszero(create(selfbalance(), 0x0b, 0x16)) { revert(codesize(), codesize()) } // For gas estimation.
+            }
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Returns the CREATE2 address of the storage contract for `data` deployed with `salt`.
+     * @param data The data that is intended to be deployed via {SSTORE2 - writeCounterFactual}.
+     * @param salt The nonce that is intended to be passed into {SSTORE2 - writeCounterfactual}.
+     * @return predicted The predicted deployed storage contract address.
+     */
+    function predictCounterfactualAddress(bytes memory data, bytes32 salt)
+        public
+        view
+        returns (address predicted)
+    {
+        predicted = SSTORE2.predictCounterfactualAddress(data, salt);
+    }
+ 
+    /**
+     * @dev Returns the "CREATE3" deterministic address for `salt`.
+     * @param salt The nonce that is intended to be passed into {SSTORE2 - writeDeterministic}.
+     * @return predicted The predicted deployed storage contract address.
+     */
+    function predictDeterministicAddress(bytes32 salt)
+        public
+        view
+        returns (address predicted)
+    {
+        predicted = SSTORE2.predictDeterministicAddress(salt);
+    }
+ 
+    /**
+     * @dev Returns `data` from `pointer`.
+     * @param pointer The deployed storage contract address.
+     * @return data The data from `pointer`.
+     */
+    function read(address pointer) public view returns (bytes memory data) {
+        data = SSTORE2.read(pointer);
+    }
+
+    /**
+     * @dev Returns the initialization code hash of the storage contract for `data`.
+     * @param data The data that is intended to be deployed via {SSTORE2 - writeCounterFactual}.
+     * @return hash The initialization code hash of the storage contract for `data`.
+     * @custom:note It is used for mining vanity addresses with create2crunch.
+     */
+    function initCodeHash(bytes memory data) public pure returns (bytes32 hash) {
+        hash = SSTORE2.initCodeHash(data);
+    }
+}
---
+++ test/SSTORE2Factory.t.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/SSTORE2Factory.sol";
+
+contract SSTORE2FactoryTest is Test {
+    SSTORE2Factory factory;
+
+    address constant _RECEIVER = 0xfa98aFe34D343D0e63C4C801EBce01d9D4459ECa;
+
+    event Pointer(uint256 index, address pointer);
+    event Pointer(address pointer);
+
+    error Unauthorized();
+    error ArrayLengthMismatch();
+
+    function setUp() public {
+        factory = new SSTORE2Factory();
+    }
+
+    function test_EmitPointer_Write() public {
+        bytes memory data = "0x123456789abcedef";
+        vm.expectEmit();
+        emit Pointer(0x104fBc016F4bb334D775a19E8A6510109AC63E00);
+        factory.write(data);
+    }
+
+    function test_EmitPointer_BatchWrite() public {
+        bytes[] memory data = _getArrayOfData();
+
+        vm.expectEmit();
+        emit Pointer(0, 0x104fBc016F4bb334D775a19E8A6510109AC63E00);
+        emit Pointer(1, 0x037eDa3aDB1198021A9b2e88C22B464fD38db3f3);
+        emit Pointer(2, 0xDDc10602782af652bB913f7bdE1fD82981Db7dd9);
+        factory.write(data);
+    }
+
+    function test_EmitPointer_WriteCounterfactual() public {
+        bytes memory data = "0x123456789abcedef";
+        bytes32 salt = 0x085cfce4666f58f44ff4b5c6b7ec5cdb72c2eb18f83600af9d2f9bd1636c6f1c;
+
+        address predicted = factory.predictCounterfactualAddress(data, salt);
+
+        vm.expectEmit();
+        emit Pointer(predicted);
+        factory.writeCounterfactual(data, salt);
+    }
+
+    function test_EmitPointer_BatchWriteCounterfactual() public {
+        bytes[] memory data = _getArrayOfData();
+        bytes32[] memory salt = _getArrayOfSalt();
+
+        address predicted0 = factory.predictCounterfactualAddress(data[0], salt[0]);
+        address predicted1 = factory.predictCounterfactualAddress(data[1], salt[1]);
+        address predicted2 = factory.predictCounterfactualAddress(data[2], salt[2]);
+
+        vm.expectEmit();
+        emit Pointer(0, predicted0);
+        emit Pointer(1, predicted1);
+        emit Pointer(2, predicted2);
+        factory.writeCounterfactual(data, salt);
+    }
+
+    function test_EmitPointer_WriteDeterministic() public {
+        bytes memory data = "0x123456789abcedef";
+        bytes32 salt = 0x085cfce4666f58f44ff4b5c6b7ec5cdb72c2eb18f83600af9d2f9bd1636c6f1c;
+
+        address predicted = factory.predictDeterministicAddress(salt);
+
+        vm.expectEmit();
+        emit Pointer(predicted);
+        factory.writeDeterministic(data, salt);
+    }
+
+    function test_EmitPointer_BatchWriteDeterministic() public {
+        bytes[] memory data = _getArrayOfData();
+        bytes32[] memory salt = _getArrayOfSalt();
+
+        address predicted0 = factory.predictDeterministicAddress(salt[0]);
+        address predicted1 = factory.predictDeterministicAddress(salt[1]);
+        address predicted2 = factory.predictDeterministicAddress(salt[2]);
+
+        vm.expectEmit();
+        emit Pointer(0, predicted0);
+        emit Pointer(1, predicted1);
+        emit Pointer(2, predicted2);
+        factory.writeDeterministic(data, salt);
+    }
+
+    function test_RevertIf_WriteCounterfactual_WhenArrayLengthOfArgumentsIsMismatch() public {
+        bytes[] memory data = _getArrayOfData();
+        bytes32[] memory salt = new bytes32[](2);
+        salt[0] = 0x085cfce4666f58f44ff4b5c6b7ec5cdb72c2eb18f83600af9d2f9bd1636c6f1c;
+        salt[1] = 0x01f96fdc02e731bed05290ce1c01891d27b1d72e321310089f2ac64e87f07271;
+
+        vm.expectRevert(ArrayLengthMismatch.selector);
+        factory.writeCounterfactual(data, salt);
+    }
+
+    function test_RevertIf_WriteDeterministic_WhenArrayLengthOfArgumentsIsMismatch() public {
+        bytes[] memory data = _getArrayOfData();
+        bytes32[] memory salt = new bytes32[](2);
+        salt[0] = 0x085cfce4666f58f44ff4b5c6b7ec5cdb72c2eb18f83600af9d2f9bd1636c6f1c;
+        salt[1] = 0x01f96fdc02e731bed05290ce1c01891d27b1d72e321310089f2ac64e87f07271;
+
+        vm.expectRevert(ArrayLengthMismatch.selector);
+        factory.writeDeterministic(data, salt);
+    }
+
+    function test_Withdraw() public {
+        uint256 contractBalanceBefore = address(factory).balance;
+        assertEq(contractBalanceBefore, 0);
+
+        hoax(address(0xA11CE), 1 ether);
+        bytes memory data = "0x123456789abcedef";
+        factory.write{value: 0.001 ether}(data);
+
+        uint256 contractBalanceAfter = address(factory).balance;
+        assertEq(contractBalanceAfter, 0.001 ether);
+
+        vm.prank(address(_RECEIVER));
+        factory.withdraw();
+
+        uint256 contractBalanceAfterWithdraw = address(factory).balance;
+        assertEq(contractBalanceAfterWithdraw, 0);
+    }
+
+    function test_RevertIf_Withdraw_WhenTheCallerIsNotReceiver() public {
+        uint256 contractBalanceBefore = address(factory).balance;
+        assertEq(contractBalanceBefore, 0);
+
+        hoax(address(0xA11CE), 1 ether);
+        bytes memory data = "0x123456789abcedef";
+        factory.write{value: 0.001 ether}(data);
+
+        uint256 contractBalanceAfter = address(factory).balance;
+        assertEq(contractBalanceAfter, 0.001 ether);
+
+        vm.prank(address(0xBAD));
+        vm.expectRevert(Unauthorized.selector);
+        factory.withdraw();
+    }
+
+    function _getArrayOfData() internal pure returns (bytes[] memory) {
+        bytes[] memory data = new bytes[](3);
+        data[0] = "0x7768617420746865206675636b";
+        data[1] = "0x123456789abcedef";
+        data[2] = "0x";
+        return data;
+    }
+
+    function _getArrayOfSalt() internal pure returns (bytes32[] memory) {
+        bytes32[] memory salt = new bytes32[](3);
+        salt[0] = 0x085cfce4666f58f44ff4b5c6b7ec5cdb72c2eb18f83600af9d2f9bd1636c6f1c;
+        salt[1] = 0x01f96fdc02e731bed05290ce1c01891d27b1d72e321310089f2ac64e87f07271;
+        salt[2] = 0x97ffde8f93071e7e114475c8259750d4f76de53b0fed8e9360280726f7de5481;
+        return salt;
+    }
+}
